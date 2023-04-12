fa397c2211a63d0d3f65a55e82daab6f09d11539
Mohammad Milhem <mohamadmilhem100@gmail.com>
2023-04-13 00:07:57 +0200

    CURRENCY Modified the Product class to add the currency type

--- PriceCalculatorKata/Products/Product.cs
+++ PriceCalculatorKata/Products/Product.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PriceCalculatorKata.Currency;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,7 +23,7 @@ namespace PriceCalculatorKata.Products
 
         public override string ToString()
         {
-            string ProductString = $"Name : {Name}, UPC : {UPC}, Base Price: ${BasePrice}";
+            string ProductString = $"Name : {Name}, UPC : {UPC}, Base Price: {BasePrice} {CurrencyFormater.CurrencyFromat(this.CurrencyType)}";
             return ProductString;
         }
 
