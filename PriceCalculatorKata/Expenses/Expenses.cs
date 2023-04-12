7df5ee3a8b80951a4c8b0d0702429cbc034d44c8
Mohammad Milhem <mohamadmilhem100@gmail.com>
2023-04-12 16:14:50 +0200

    Adding IExpenses interface

--- PriceCalculatorKata/Expenses/Expenses.cs
+++ PriceCalculatorKata/Expenses/Expenses.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 
 namespace PriceCalculatorKata.Expenses
 {
-    public class Expenses
+    public class Expenses : IExpenses
     {
         public string Description { get; set; }
-        public decimal Amount { get; set; } 
+        public decimal Amount { get; set; }
         public ExpenseType ExpenseType { get; set; }
 
         public Expenses(decimal Amount, ExpenseType ExpenseType, string Description)
-        { 
+        {
             this.Amount = Amount;
             this.ExpenseType = ExpenseType;
             this.Description = Description;
---
+++ PriceCalculatorKata/Expenses/IExpenses.cs
@@ -0,0 +1,9 @@
+﻿namespace PriceCalculatorKata.Expenses
+{
+    public interface IExpenses
+    {
+        decimal Amount { get; set; }
+        string Description { get; set; }
+        ExpenseType ExpenseType { get; set; }
+    }
+}
