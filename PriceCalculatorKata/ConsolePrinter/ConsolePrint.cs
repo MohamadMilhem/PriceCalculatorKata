106e624bd26f09da37e9e6a233c5fa493b900787
Mohammad Milhem <mohamadmilhem100@gmail.com>
2023-04-12 17:07:04 +0200

    Added a ExpensesReport method

--- PriceCalculatorKata/ConsolePrinter/ConsolePrint.cs
+++ PriceCalculatorKata/ConsolePrinter/ConsolePrint.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using PriceCalculatorKata.Expenses;
 using PriceCalculatorKata.Products;
 
 namespace PriceCalculatorKata.ConsolePrinter
@@ -24,6 +25,23 @@ namespace PriceCalculatorKata.ConsolePrinter
             Console.WriteLine(Product.ToString());
             Console.WriteLine($"Base Price : ${Calculations.FinalPrice()}");
             Console.WriteLine($"Discount Price: ${Calculations.GetTotalDiscount()}");
+        }
+
+        public void ReportExpenses()
+        {
+            Console.WriteLine($"Cost = ${Product.BasePrice}");
+            Console.WriteLine($"Tax = ${Calculations.GetTotalTax()}");
+            Console.WriteLine($"Discount = ${Calculations.GetTotalDiscount()}");
+
+            var Expenses = new ExpensesRepository().GetExpensesByUPC(Product.UPC);
+            
+            foreach (var expense in Expenses)
+            {
+                Console.WriteLine($"{expense.Description} = ${Calculations.ExpenseAmount(expense)}");
+            }
+
+            Console.WriteLine($"Total = ${Calculations.FinalPrice()}");
+
         }
 
 
