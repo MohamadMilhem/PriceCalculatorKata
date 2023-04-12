aec0abfcbf8a934e8b15d66f9aeb1c09ff78120f
Mohammad Milhem <mohamadmilhem100@gmail.com>
2023-04-12 16:31:19 +0200

    EXPENSES Rename Expenses.cs to Expense.cs and Added a Property ProductUCP

---
+++ PriceCalculatorKata/Expenses/Expense.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PriceCalculatorKata.Expenses
+{
+    public class Expense : IExpenses
+    {
+        public string Description { get; set; }
+        public decimal Amount { get; set; }
+        public ExpenseType ExpenseType { get; set; }
+
+        public long ProductUPC { get; set; }
+
+        public Expense(decimal Amount, ExpenseType ExpenseType, string Description)
+        {
+            this.Amount = Amount;
+            this.ExpenseType = ExpenseType;
+            this.Description = Description;
+        }
+
+    }
+}
