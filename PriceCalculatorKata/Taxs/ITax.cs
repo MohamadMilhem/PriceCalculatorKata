e3fc6d7fee6b4184d765e70f8fffaa92fd40df4b
Mohammad Milhem <mohamadmilhem100@gmail.com>
2023-04-17 16:11:51 +0300

    Added ITax and ITaxRepository

---
+++ PriceCalculatorKata/Taxs/ITax.cs
@@ -0,0 +1,11 @@
+﻿namespace PriceCalculatorKata.Taxs
+{
+    public interface ITax
+    {
+        bool IsUniversal { get; set; }
+        long ProductUPC { get; set; }
+        decimal TaxPercentage { get; set; }
+
+        string ToString();
+    }
+}
---
+++ PriceCalculatorKata/Taxs/ITaxRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace PriceCalculatorKata.Taxs
+{
+    public interface ITaxRepository
+    {
+        List<Tax> GetAll();
+        IEnumerable<Tax> GetTaxesByUPC(long UPC);
+    }
+}
